5c04799c1570683bd288e210fd118ec4bb33f375
Lesedi Pitsi <lesedipitsi@gmail.com>
2020-02-21 13:15:21 +0200

    First commit

---
+++ app/src/main/java/apps/studios/bt/mrdfoodclone/MapActivity.kt
@@ -0,0 +1,13 @@
+package apps.studios.bt.mrdfoodclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+    }
+}
