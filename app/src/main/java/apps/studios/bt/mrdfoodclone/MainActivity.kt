a959e573542aff4ba02fb7489d64b2fb1ca8d01b
Lesedi Pitsi <lesedipitsi@gmail.com>
2020-02-22 13:47:20 +0200

    Updated Map UI

--- app/src/main/java/apps/studios/bt/mrdfoodclone/MainActivity.kt
+++ app/src/main/java/apps/studios/bt/mrdfoodclone/MainActivity.kt
@@ -1,12 +1,18 @@
 package apps.studios.bt.mrdfoodclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        deliver.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
     }
 }
--- app/src/main/java/apps/studios/bt/mrdfoodclone/MapActivity.kt
+++ app/src/main/java/apps/studios/bt/mrdfoodclone/MapActivity.kt
@@ -3,6 +3,7 @@ package apps.studios.bt.mrdfoodclone
 import android.graphics.Point
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Looper
@@ -12,6 +13,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import apps.studios.bt.mrdfoodclone.views.DraggableMap
 import apps.studios.bt.mrdfoodclone.views.MapWrapper
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
@@ -24,9 +28,11 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapWrapper.OnDragLi
 
     private lateinit var locationTextView: TextView
     private lateinit var googleMapFragment: DraggableMap
+    private var lastLocation: Location? = null
+    private lateinit var fusedLocation: FusedLocationProviderClient
     private var googleMap: GoogleMap? = null
-    private var coord_marker_x : Int = 0
-    private var coord_marker_y : Int = 0
+    private var coord_marker_x: Int = 0
+    private var coord_marker_y: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +40,11 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapWrapper.OnDragLi
         initComponents()
     }
 
-    private fun initComponents()
-    {
+    private fun initComponents() {
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocation.lastLocation.addOnSuccessListener {
+            lastLocation = it
+        }
         locationTextView = sheet_location
         coord_marker_x = map_pointer?.x!!.toInt()
         coord_marker_y = map_pointer?.bottom!!.plus(map_pointer?.width!!.div(2))
@@ -44,24 +53,50 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapWrapper.OnDragLi
         googleMapFragment.getMapAsync(this)
         googleMapFragment.setOnMapDragListener(this)
 
+
     }
 
-    private fun updateLocation(coords: LatLng)
-    {
+    private fun updateLocation(coords: LatLng) {
         GetGoogleMapAddress(this).execute(coords)
     }
 
     override fun onMapReady(map: GoogleMap?) {
         googleMap = map
 
+        googleMap?.uiSettings!!.isMyLocationButtonEnabled = false
+        googleMap?.setMaxZoomPreference(18f)
+        if (lastLocation != null) {
+            googleMap?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(lastLocation?.latitude!!, lastLocation?.longitude!!),
+                    17f
+                )
+
+            )
+        } else {
+            googleMap?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(-34.2788339, 18.2493811), 17f
+                )
+            )
+        }
+// LatLng(-34.2788339, 18.2493811), 17f)
     }
 
     override fun onMapDrag(motionEvent: MotionEvent?) {
-
+        locationTextView.text = getString(R.string.loading)
+        //   map_pointer?.animate()!!.translationY(1f).start()
     }
 
     override fun onMapRelease(action_release: Int?) {
-        updateLocation(googleMap?.projection!!.fromScreenLocation(Point(coord_marker_x, coord_marker_y)))
+        updateLocation(
+            googleMap?.projection!!.fromScreenLocation(
+                Point(
+                    coord_marker_x,
+                    coord_marker_y
+                )
+            )
+        )
     }
 
     override fun onResume() {
@@ -69,19 +104,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapWrapper.OnDragLi
         googleMap?.clear()
     }
 
-    companion object
-    {
+    companion object {
         class GetGoogleMapAddress(private val activity: MapActivity) :
-            AsyncTask<LatLng, Void, String>()
-        {
+            AsyncTask<LatLng, Void, String>() {
 
             init {
                 if (Looper.myLooper() == null) Looper.prepare()
             }
 
-            override fun onPreExecute() {
-                activity.locationTextView.text = activity.getString(R.string.loading)
-            }
 
             override fun doInBackground(vararg latLng: LatLng): String? {
 
@@ -90,29 +120,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapWrapper.OnDragLi
 
                     val geocoder = Geocoder(activity, Locale.getDefault())
 
-                    val addressList = geocoder.getFromLocation(latLng[0].latitude, latLng[0].longitude, 1)
+                    val addressList =
+                        geocoder.getFromLocation(latLng[0].latitude, latLng[0].longitude, 1)
 
-                    if(addressList?.size!! > 0)
-                    {
+                    if (addressList?.size!! > 0) {
                         val obj: Address = addressList[0]
 
-                        completerAddress.append(obj.featureName).append(", ").append(obj.countryCode)
+                        completerAddress.append(
+                            obj.getAddressLine(0).substring(
+                                0,
+                                obj.getAddressLine(0).indexOf(',')
+                            )
+                        )
+                            .append(", ").append(obj.countryName)
 
                     }
                     return completerAddress.toString()
 
-                }
-                catch (e: IOException)
-                {
+                } catch (e: IOException) {
                     return null
                 }
             }
 
             override fun onPostExecute(completeAddress: String?) {
 
-                if(completeAddress == null)
-                {
+                if (completeAddress == null) {
                     Toast.makeText(activity, R.string.google_maps_error, Toast.LENGTH_LONG).show()
+                    activity.locationTextView.text = activity.getString(R.string.sample_location)
+
                     return
                 }
                 activity.locationTextView.text = completeAddress
